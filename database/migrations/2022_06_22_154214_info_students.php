bbe294f1d708d21c6b19eef4a0f6b924a93a3d1a
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2022-06-22 23:07:30 +0700

    Initial commit

---
+++ database/migrations/2022_06_22_154214_info_students.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InfoStudents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info_students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('hoten');
+            $table->string('gioitinh');
+            $table->date('ngaysinh');
+            $table->integer('sdt');
+            $table->string('diachi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
